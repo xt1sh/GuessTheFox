60df55e656b4e74ed9f0e4b83ac7b281f85ba4f8
xt1sh <azylav@gmail.com>
2020-07-18 16:44:31 +0300

    ng material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { GameModule } from './game-module/game.module';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    GameModule
+    GameModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
