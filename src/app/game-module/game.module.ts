c6dcb886a4adea6f275cc8a1537014811acbd0f6
Ivan Koval <azylav@gmail.com>
2020-07-18 03:23:45 +0300

    game module add

---
+++ src/app/game-module/game.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GameComponent } from './game-component/game.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [GameComponent]
+})
+export class GameModule { }
