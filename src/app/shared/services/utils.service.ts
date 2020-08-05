f05eaba26213147f434bfd70f0c016a4babcfae8
xt1sh <azylav@gmail.com>
2020-08-06 01:21:35 +0300

    some game mechanics

---
+++ src/app/shared/services/utils.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilsService {
+
+	constructor() { }
+
+	shuffle<T>(arr: T[]): T[] {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+}
